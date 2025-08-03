65c1a5f144dc8d298a66735ca70848abeab3f48f
Supriya22IT58 <126750703+Supriya22IT58@users.noreply.github.com>
2025-08-03 18:13:48 +0530

    Create comment.js

---
+++ backend/models/comment.js
@@ -0,0 +1,7 @@
+const CommentSchema = new mongoose.Schema({
+  postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  content: String,
+  createdAt: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model('Comment', CommentSchema);
